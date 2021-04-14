ad91ff47af319718aefd769c73dc11bae81babe9
Md. Soliman Ali <65532591+Md-Soliman-Ali@users.noreply.github.com>
2021-04-14 14:48:19 +0600

    Add files via upload

---
+++ Basic Code/List (Append, Insert, Pop, Delete, Clear).py
@@ -0,0 +1,20 @@
+list = ["A", "B", "D", "M", "N", "O"]
+print("Add Using Append Method ")
+list.append("L")
+print(list)
+
+print("Add Using Insert Method For Specific Position ")
+list.insert(2, "C")
+print(list)
+
+print("Remove Last Element Using Pop Method ")
+list.pop()
+print(list)
+
+print("Remove Specific Position Using Del Method ")
+del list[0]
+print(list)
+
+print("Clear All Data Using Clear Method ")
+list.clear()
+print(list)
