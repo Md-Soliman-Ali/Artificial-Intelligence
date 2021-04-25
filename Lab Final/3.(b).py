b71a0dc72752818cdc778f0c0c70de64881e8003
Md. Soliman Ali <65532591+Md-Soliman-Ali@users.noreply.github.com>
2021-04-25 10:12:32 +0600

    Add files via upload

---
+++ Lab Final/3.(b).py
@@ -0,0 +1,20 @@
+list = ["1", "2", "3", "4", "5", "6"]
+print("Adding Using Append Method ")
+list.append("7")
+print(list)
+
+print("Adding Using Insert Method For Specific Position(2) ")
+list.insert(2, "2.5")
+print(list)
+
+print("Removeing Last Element Using Pop Method ")
+list.pop()
+print(list)
+
+print("Removeing Specific Position (0) Using Del Method ")
+del list[0]
+print(list)
+
+print("Clearing All Data Using Clear Method ")
+list.clear()
+print(list)
