9e064ae47c8a49aac49dce2534acf6593885dae9
Md. Soliman Ali <65532591+Md-Soliman-Ali@users.noreply.github.com>
2021-04-26 10:26:02 +0600

    Add files via upload

---
+++ Lab Final/4.(a).py
@@ -0,0 +1,8 @@
+first_set = {20, 22, 24, 26, 28, 30}
+second_set = {10, 20, 22, 40, 50, 60}
+
+print("Union :", first_set | second_set)
+
+print("Intersection :", first_set & second_set) 
+
+print("Set Difference :", first_set - second_set)
